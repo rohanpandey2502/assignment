77cf6037c55618e733ffbc35c9da83f5cfaf9f55
rohanpandey2502 <121759061+rohanpandey2502@users.noreply.github.com>
2023-01-01 20:30:09 +0530

    Add files via upload

---
+++ Fibonacci series-rohan.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Fibonacci series
+
+a=0
+b=1
+c=0
+print(a)
+print(b)
+for i in range(0,8):
+    c=a+b
+    a=b
+    b=c
+    print(c)
+
+
+# In[ ]:
+
+
+
+
